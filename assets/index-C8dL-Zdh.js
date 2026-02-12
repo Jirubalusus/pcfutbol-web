06fe930b953750f98040d9d5209f4a960afc36de
Jiru <jirubalusus@gmail.com>
2026-02-12 10:54:29 +0100

    Updates

---
+++ assets/index-C8dL-Zdh.js
@@ -0,0 +1,3 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BnC4FLrL.js","assets/index-Ck-rA4VO.js","assets/index-Bl2pLs3k.css"])))=>i.map(i=>d[i]);
+import{r as o,_ as t}from"./index-Ck-rA4VO.js";const n=o("SocialLogin",{web:()=>t(()=>import("./web-BnC4FLrL.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SocialLoginWeb)});export{n as SocialLogin};
+//# sourceMappingURL=index-C8dL-Zdh.js.map
--- assets/index-cLtUwvcM.js
+++
@@ -1,3 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Dx3OwaEp.js","assets/index-DKig0FDC.js","assets/index-DXyGyKHm.css"])))=>i.map(i=>d[i]);
-import{r as p,_ as r}from"./index-DKig0FDC.js";const _=p("App",{web:()=>r(()=>import("./web-Dx3OwaEp.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
-//# sourceMappingURL=index-cLtUwvcM.js.map
