0740a7010fca95e5ccb1224a4a4906f99fb5f644
Jiru <jirubalusus@gmail.com>
2026-02-12 10:46:22 +0100

    Updates

---
+++ assets/index-BP0I049_.js
@@ -0,0 +1,3 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-D08pBt3k.js","assets/index-DKig0FDC.js","assets/index-DXyGyKHm.css"])))=>i.map(i=>d[i]);
+import{r as o,_ as t}from"./index-DKig0FDC.js";const n=o("SocialLogin",{web:()=>t(()=>import("./web-D08pBt3k.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SocialLoginWeb)});export{n as SocialLogin};
+//# sourceMappingURL=index-BP0I049_.js.map
