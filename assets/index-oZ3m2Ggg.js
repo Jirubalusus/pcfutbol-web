abe10be7b469270df9695fa2791ca51fa3b5122e
Jiru <jirubalusus@gmail.com>
2026-02-12 11:57:17 +0100

    Updates

--- assets/index-oZ3m2Ggg.js
+++
@@ -1,3 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BCk9QD9c.js","assets/index-D5P5878y.js","assets/index-B0T56xMM.css"])))=>i.map(i=>d[i]);
-import{r as o,_ as t}from"./index-D5P5878y.js";const n=o("SocialLogin",{web:()=>t(()=>import("./web-BCk9QD9c.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SocialLoginWeb)});export{n as SocialLogin};
-//# sourceMappingURL=index-oZ3m2Ggg.js.map
