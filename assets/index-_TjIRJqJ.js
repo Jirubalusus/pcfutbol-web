6b7689548ae58d6a70c1acd33a36cff5b11e2309
Jiru <jirubalusus@gmail.com>
2026-02-12 10:08:11 +0100

    Updates

---
+++ assets/index-_TjIRJqJ.js
@@ -0,0 +1,3 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BPMQXYtz.js","assets/index-C4NeMn89.js","assets/index-DEhXqXDY.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-C4NeMn89.js";const _=p("App",{web:()=>r(()=>import("./web-BPMQXYtz.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
+//# sourceMappingURL=index-_TjIRJqJ.js.map
