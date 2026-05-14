5d0eac023b0ec38fd9d18ebb7461b370bbb3ff7d
Jiru <jirubalusus@gmail.com>
2026-05-14 13:32:38 +0200

    Updates

---
+++ assets/index-BRxAKeQI.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-VLdmdiNg.js","assets/firebase-CKiOS6Ay.js"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./firebase-CKiOS6Ay.js";const _=p("App",{web:()=>r(()=>import("./web-VLdmdiNg.js"),__vite__mapDeps([0,1])).then(e=>new e.AppWeb)});export{_ as App};
